a946853380cd381c534d65abaf77e93da3366015
acancho <andycancho@close2u.pe>
2025-12-12 16:02:04 -0500

    rename refactor

--- app/docker/run_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. ./set_env_secrets.sh
-exec uvicorn main:app --host 0.0.0.0 --port 8014
-
---
+++ docker/run_all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+. ./set_env_secrets.sh
+exec uvicorn main:app --host 0.0.0.0 --port 8000
+
